c67eea9de9c2f01b3f09fae22b2e5878322f3ed3
Graitavicius <grait.mindaugas@gmail.com>
2020-07-04 17:34:11 +0300

    added form groups with controls, radio buttons, now you can also use the data from the form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,9 +10,34 @@ export class AppComponent {
   @ViewChild("f", { static: false }) signupForm: NgForm;
   defaultQuestion = "pet";
   answer = "";
+  genders = ["Male", "Female", "Other"];
+  user = {
+    username: "",
+    email: "",
+    secretQuestion: "",
+    answer: "",
+    gender: "",
+  };
+  submitted = false;
 
   suggestUserName() {
     const suggestedName = "Superuser";
+    // this.signupForm.setValue({ - set value for the entire form
+    //   userData: {
+    //     username: suggestedName,
+    //     email: "",
+    //   },
+    //   secret: "pet",
+    //   questionAnswer: "",
+    //   gender: "male",
+    // });
+
+    this.signupForm.form.patchValue({
+      // - patch value only overwrites parts of the form
+      userData: {
+        username: suggestedName,
+      },
+    });
   }
 
   // onSubmit(form: NgForm) {
@@ -20,6 +45,13 @@ export class AppComponent {
   // }
 
   onSubmit() {
-    console.log(this.signupForm);
+    this.submitted = true;
+    this.user.username = this.signupForm.value.userData.username;
+    this.user.email = this.signupForm.value.userData.email;
+    this.user.secretQuestion = this.signupForm.value.secret;
+    this.user.answer = this.signupForm.value.questionAnswer;
+    this.user.gender = this.signupForm.value.gender;
+
+    this.signupForm.reset();
   }
 }
